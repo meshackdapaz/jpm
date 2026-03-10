aa18980a34644f96158a4fe988bd56a34780d73a
Joshan <joshan@example.com>
2026-03-11 01:07:09 +0300

    Fix iOS crash: call ApplicationDelegateProxy in didFinishLaunching

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        return ApplicationDelegateProxy.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
